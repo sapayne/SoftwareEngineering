52838f78986f833e05973e2e7d5d2e7bece6d590
sapayne <sapayne007@gmail.com>
2017-10-23 20:30:09 -0400

    database Updates
    what was coded monday octember 23 2017

--- src/Database/bPlusTree.java
+++ src/Database/bPlusTree.java
@@ -12,49 +12,109 @@ public class bPlusTree {
 	public bPlusTree(int length) {
 		treeRoot = new bTreeNode(length);
 		this.length = length;
+		size = 0;
 	}
 	
 	public int getLength() {
 		return length;
 	}
 	
-	// if the bTree node can no longer contain any more nodes, then the bTree node is split into two bTree nodes
-	// where the values less than the middle index are stored in the first newly created bTree node, and the values
-	// greater than or equal to the middle value are stored in the second newly created bTree node. Then in the 
-	// parent node that was just split, the value to the left of the middle value goes from a node pointer to a 
-	// bTree node pointer
+	/* if the bTree node can no longer contain any more nodes, then the bTree node is split into two bTree nodes
+	 * where the values less than the middle index are stored in the first newly created bTree node, and the values
+	 * greater than or equal to the middle value are stored in the second newly created bTree node. Then in the 
+	 * parent node that was just split, the value to the left of the middle value goes from a node pointer to a 
+	 * bTree node pointer, which points to the less than bTree node and the value to the right goes to the greater
+	 * than bTree node. Last it calls leafUpdate function which connects all the leaf bTree nodes together.
+	 */
 	private void split(bTreeNode nodeToSplit) {
 		bTreeNode lessThan = new bTreeNode(length);
 		bTreeNode greaterThanEqual = new bTreeNode(length);
-		for(int i = 0; i < length/2+1; i++) {
-			lessThan.children[i] = nodeToSplit.children[i];
-			greaterThanEqual.children[length/2-i] = nodeToSplit.children[length-1-i];
+		for(int i = 0; i < length/2; i++) {
+			lessThan.contents[i] = nodeToSplit.contents[i];
+			greaterThanEqual.contents[length/2-i] = nodeToSplit.contents[length-1-i];
 		}
+		lessThan.setSize(length/2);
+		greaterThanEqual.setSize(length/2);
+		if((length/2)%2 == 1) {
+			greaterThanEqual.contents[0] = nodeToSplit.contents[length/2+1];
+			greaterThanEqual.incSize();
+		}
+		
+		if(nodeToSplit.getParent() == null) {
+			
+		}
+		
 		lessThan.setParent(nodeToSplit);
+		lessThan.setRightSibling(greaterThanEqual);
 		greaterThanEqual.setParent(nodeToSplit);
+		greaterThanEqual.setLeftSibling(lessThan);
+		nodeToSplit.addChild(lessThan);
+		nodeToSplit.addChild(greaterThanEqual);
 	}
 	
 	//inserts and sorts the new item to the database, if the item doesn't already exist
 	public void insort(String name, String index) {
-		//call search to see if it exists, if not add it to the bPlusTree
-		current = binarySearch(name);
-		if(current == null) {
-			current.addChild(Node);
-		}
-		if (current.size() == current.getLength()) {
-			split(current);
-		} else {
+		if(size() == 0) {
+			treeRoot = new bTreeNode(length);
+			current = treeRoot;
 			Node.setName(name);
 			Node.setIndex(index);
 			current.addChild(Node);
 			current.incSize();
 			this.size++;
+		} else {
+			//call search to see if it exists, if not add it to the bPlusTree
+			current = binarySearch(name);
+			if(current == null) {
+				current.addChild(Node);
+			}
+			if (current.size() == current.getLength()) {
+				split(current);
+			} else {
+				Node.setName(name);
+				Node.setIndex(index);
+				current.addChild(Node);
+				current.incSize();
+				this.size++;
+			}
 		}
+	}
+	
+	public boolean search(String name) {
+		int index = treeRoot.binarySearch(name, 0, treeRoot.size());
+		if(index == -1) {
+			if(treeRoot.children[index+1] instanceof bTreeNode) {
+				
+			}
+		}
+		if(name.equalsIgnoreCase(treeRoot.contents[index])) {
+			
+		}
+		return true;
+	}
+	
+	public boolean search(String name, bTreeNode current) {
+		
+		return false;
 	}
 	
 	//allows you to find the bTree node closest to the value you want to add or find in the bPlusTree
 	public bTreeNode binarySearch(String name) {
 		//finds the object by name in the database, and returns the key which will be the Index.
+		
+		if(isLeaf(treeRoot)) {
+			if((treeRoot.size()/2)%2 != 1) {
+				if(name.compareToIgnoreCase(current.nodeArray((treeRoot.size()/2+1))) < 0) {
+					
+				}
+			
+			} else {
+				current.nodeArray((current.size()/2));
+			}
+			
+		}
+		
+		
 		if((current.size()/2)%2 != 1) {
 			current.nodeArray((treeRoot.size()/2+1));
 		} else {
@@ -64,22 +124,50 @@ public class bPlusTree {
 		return current;
 	}
 	
-	public void transverse() {
+	//goes as far down to the left as possible then starts printing values from the furtherest left to the 
+	//furtherest right
+	public void inOrderTransverse() {
+		current = treeRoot;
+		while(current.children[0] instanceof bTreeNode) {
+			current = (bTreeNode) current.children[0];
+		}
 		
+		System.out.print("The B+ tree printed in order is: ");
+		while(current != null) {						//current.getRightSibling() if this causes an error
+			for(int i = 0; i < current.size(); i++) {	//loops to print out all contents in that bTree node
+				System.out.print(current.children[i]);	
+			}
+			current = current.getRightSibling();		//progressively from left to right printing out all leaf nodes
+		}
+	}
+	
+	//might not need 
+	public void leafUpdate() {
+		current = treeRoot;
+		while(current.children[0] instanceof bTreeNode) {
+			current = (bTreeNode) current.children[0];
+		}
+		current = current.getParent();
+	}
+	
+	public void rootUpdate() {
+		while(treeRoot.getParent() != null) {
+			treeRoot = treeRoot.getParent();
+		}	
 	}
 	
 	//deletes the item from the database
-	public void delete(String itemName) {
-		this.size--;
+	public void delete(String name) {
+		size--;
 		//searches for the item by name then removes it from the bPlusTree
 	}
 	
 	public int size() {
-		return this.size;
+		return size;
 	}
 	
-	public boolean isLeaf() {
-		return treeRoot.children[0] instanceof node;
+	public boolean isLeaf(bTreeNode current) {
+		return current.children[0] == null;
 	}
 	
 	public boolean isEmpty() {
--- src/Database/bTreeNode.java
+++ src/Database/bTreeNode.java
@@ -6,51 +6,99 @@ public class bTreeNode {
 	private bTreeNode parent;
 	private bTreeNode rightSibling;
 	private bTreeNode leftSibling;
-	private node current;
-	
+	public String[] contents;
 	public Object[] children;
 	private int size; 			//size of treeNode, which is equal to the amount of nodes in it
 	
 	public bTreeNode(int length) {
+		parent = null;
 		/* 	Siblings are only used if the bTreeNode has no children, also known as a leaf node, as
 		 * 	this will connect the lowest level nodes together as a way of returning the range of 
 		 *	numbers between to indices.
 		 */
-		this.rightSibling = null;
-		this.leftSibling = null;
+		rightSibling = null;
+		leftSibling = null;
 		/* 	creates the array that will hold the pointers to the bTree nodes that contain the strings and nodes, 
 		 *	or holds pointers to the nodes themselves along with their string values, bTree nodes and nodes are
 		 *	never present in the same array.
 		 */
-		children = new Object[2*length+1];
+		contents = new String[length];
+		children = new Object[length+1];
+		size = 0;
 	}
 	
 	//adds the pointer to the node containing the object information and the name of the object in the next index
 	public void addChild(Object nodeToTest) {
-		children[size()] = nodeToTest;
-		if(children[size()] instanceof node) {
-			current = (node)children[size()];
-			children[size() + 1] = current.getName();
+		int index;
+		if(nodeToTest instanceof node) {
+			index = search(((node) nodeToTest).getName());
+		} 
+		if(nodeToTest instanceof bTreeNode) {
+			
 		}
+	}
+	
+	//searches through the bTreeNode and returns the index where the name would be found, so if it's less than 
+	//every name it should return 0 for the index, and if it's greater than every name it should return length
+	//of the children array.
+	public int search(String name) {
+		int index = 0;
+		for(int i = 0; i < size(); i+=2) {
+				if(name.compareToIgnoreCase(nodeArray(i)) < 0) {
+					index = i-1;
+					break;
+				} else if (name.compareToIgnoreCase(nodeArray(i)) >= 0) {
+					index = i;
+				} 
+		}
+		return index;
+	}
+	
+	//only works if the array is all of type string, since if iterated through an object array it will return
+	//the java provided name for the pointer in memory thus leading to some unwanted results.
+	public int binarySearch(String name, int startingPointer, int endingPointer) {
+		int index = 0;
+		if(endingPointer-startingPointer > 1) {
+			if(name.compareToIgnoreCase(nodeArray((endingPointer-startingPointer)/2 + startingPointer)) < 0) {
+				return binarySearch(name, startingPointer, (endingPointer-startingPointer)/2 + startingPointer);
+			} else if(name.compareToIgnoreCase(nodeArray((endingPointer-startingPointer)/2 + startingPointer)) > 0) {
+				return binarySearch(name, (endingPointer-startingPointer)/2 + startingPointer, endingPointer);
+			} else {
+				index = (endingPointer-startingPointer)/2 + startingPointer;
+			}
+		} else {
+			if(name.compareToIgnoreCase(nodeArray((endingPointer-startingPointer)/2 + startingPointer)) < 0) {
+				index = (endingPointer-startingPointer)/2 + startingPointer - 1;
+			} else {
+				index = (endingPointer-startingPointer)/2 + startingPointer;
+			}
+		}
+		return index;
 	}
 	
 	//returns the length the children array, that is used to hold the strings, nodes, and bTree nodes.
 	public int getLength() {
-		return this.children.length;
+		return children.length;
 	}
 	
 	// increases the counter of the number of nodes presently in the bTree node
 	public void incSize() {
-		this.size++;
+		size++;
 	}
 	
 	//returns the number of nodes in the bTree node
 	public int size() {
-		return this.size;
+		return size;
 	}
-
-	public void nodeArray(int index) {
-		
+	
+	//allows the split method in the bPlusTree class to set the size of each child node
+	public void setSize(int size) {
+		this.size = size;	
+	}
+	
+	//returns the string value located at the index passed
+	public String nodeArray(int index) {
+		return contents[index];
 	}
 
 	//returns if the bTree node has nodes in it or not
@@ -65,31 +113,31 @@ public class bTreeNode {
 	
 	//returns the parent of the current bTree node selected
 	public bTreeNode getParent() {
-		return this.parent;
+		return parent;
 	}
 	
 	//sets the right sibling to the next leaf bTree node
 	public void setRightSibling(bTreeNode sibling) {
-		this.rightSibling = sibling;
+		rightSibling = sibling;
 	}
 	
 	/*	returns the leaf bTree node that is connected to the current bTree node's right pointer,
 	 *	the bTree node that contains elements less than the current bTree node.
 	 */
 	public bTreeNode getRightSibling() {
-		return this.rightSibling;
+		return rightSibling;
 	}
 	
 	//sets the left sibling to the previous leaf bTree node
 	public void setLeftSibling(bTreeNode sibling) {
-		this.leftSibling = sibling;
+		leftSibling = sibling;
 	}
 	
 	/*	returns the leaf bTree node that is connected to the current bTree node's left pointer, 
 	 *	the BTree node that contains elements less than the current bTree node. 
 	 */
 	public bTreeNode getLeftSibling() {
-		return this.leftSibling;
+		return leftSibling;
 	}
 	
 }
