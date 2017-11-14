f1bf9e03e65741b3d95424b5de0a77bbe1907ef5
dzheng33 <31329935+dzheng33@users.noreply.github.com>
2017-11-14 00:03:11 -0500

    Add files via upload

---
+++ GUINile/src/Database/Product.java
@@ -0,0 +1,49 @@
+package softwareengineering;
+
+public class Product {
+	
+	private String cardName;
+	private String cardNum;
+	private String cardExdate;
+	
+	public Product() {
+		this.cardName = "";
+		this.cardNum = "";
+		this.cardExdate = "";
+
+		}
+	
+	public Product(String cardName, String cardNum, String cardExdate) {
+		this.cardName = cardName;
+		this.cardNum = cardNum;
+		this.cardExdate = cardExdate;
+		
+	}
+
+	public String getCardName() {
+		return cardName;
+	}
+
+	public void setCardName(String cardName) {
+		this.cardName = cardName;
+	}
+
+	public String getCardNum() {
+		return cardNum;
+	}
+
+	public void setCardNum(String cardNum) {
+		this.cardNum = cardNum;
+	}
+
+	public String getCardExdate() {
+		return cardExdate;
+	}
+
+	public void setCardExdate(String cardExdate) {
+		this.cardExdate = cardExdate;
+	}
+	
+	
+
+}
